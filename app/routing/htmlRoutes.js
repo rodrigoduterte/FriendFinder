c0ab78d072a04afcbcb31c261cd43eb2c7f10e60
rodrigoduterte <gabohermoso@gmail.com>
2018-08-11 21:38:55 -0400

    final files

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,16 +1,18 @@
-var dir =  __dirname.split('\\');
-dir[dir.length-1] = 'public';
-dir = dir.join('\\');
 var path = require('path');
+
+var dir =  __dirname.split(path.sep);
+dir[dir.length-1] = 'public';
+dir = path.join(...dir);
+
 
 module.exports = function(app){
     // ROUTES
     app.get('/survey',function(req,res){
-        // console.log(path.join(dir,'survey.html'));
+        console.log(path.join(dir,'survey.html'));
         res.sendFile(path.join(dir,'survey.html'));
     });
     app.get('/',function(req,res){
-        // console.log(path.join(dir,'home.html'));
+        console.log(path.join(dir,'home.html'));
         res.sendFile(path.join(dir,'home.html'));
     });
 }
